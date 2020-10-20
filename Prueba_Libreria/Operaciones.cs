b2bedeb3d7b6e12119a2f94a63a6baaf9997d648
javiermoreno18 <72530066+javiermoreno18@users.noreply.github.com>
2020-10-19 17:54:05 -0700

    Add files via upload

---
+++ Prueba_Libreria/Operaciones.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prueba_Libreria
+{
+    public class Operaciones
+    {
+        public static double suma(double numero1, double numero2)
+        {
+            return numero1 + numero2;
+        }
+        public static double resta(double numero1, double numero2)
+        {
+            return numero1 - numero2;
+        }
+        public static double multiplicacion(double numero1, double numero2)
+        {
+            return numero1 * numero2;
+        }
+        public static double division(double numero1, double numero2)
+        {
+            return numero1 / numero2;
+        }
+    }
+}
